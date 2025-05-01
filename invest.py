12ee4707cacd59f7a264863c21bc69c2943a0020
yongdragonee <yongdragonee@gmail.com>
2025-05-01 22:12:16 +0900

    Update invest.py

--- invest.py
+++ invest.py
@@ -101,7 +101,7 @@ def format_report(idx):
             📌 {one_line_summary}<br> </span>
           <span style="font-size: 14px;"> 
             🔑 {keywords}<br>
-            💾 {filesize_info} Byte<br>
+            💾 {filesize_info} Byte<br><br>
           </span>
         </summary>
       <div style='margin-top: 11px; padding-left: 11px;'>
